7ed02af54254ea0075f53960b9fb73345d5aa9d4
Diego Gliosca <diego.gliosca@gmail.com>
2024-11-14 20:18:58 +0000

    there are no available auctions to bid

--- auction-service/src/test/kotlin/com/merchantsolutions/AuctionServerTest.kt
+++ auction-service/src/test/kotlin/com/merchantsolutions/AuctionServerTest.kt
@@ -7,11 +7,16 @@ import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.junit.jupiter.api.Test
 import com.merchantsolutions.AuctionJson.auto
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.http4k.core.Method.GET
+import org.http4k.routing.RoutingHttpHandler
 
 class AuctionServerTest {
     private val auctionServer: HttpHandler = auctionApp()
 
     private val seller = Seller(auctionServer)
+    private val buyer = Buyer(auctionServer)
 
     class Seller(val client: HttpHandler) {
 
@@ -26,10 +31,35 @@ class AuctionServerTest {
         }
     }
 
+    class Buyer(val client: HttpHandler) {
+        fun listAuctions() = activeAuctions(client(Request(GET, "/active-auctions")))
+
+        private val activeAuctions = Body.auto<List<Auction>>().toLens()
+    }
+
     @Test
     fun `seller can register a new product`() {
         seller.registerProduct()
     }
+
+    @Test
+    fun `there are no auction to bid`() {
+        val auctionList = buyer.listAuctions()
+        assertThat(auctionList, equalTo(emptyList()))
+    }
 }
 
-fun auctionApp() = routes("/register-product" bind POST to { Response(OK) })
+fun auctionApp(): RoutingHttpHandler {
+    return routes(
+        "/register-product" bind POST to { Response(OK) },
+        "/active-auctions" bind GET to { Response(OK).with(AuctionResult.lens of listOf()) }
+    )
+}
+
+class Auction
+class AuctionResult {
+    companion object {
+        val lens = Body.auto<List<Auction>>().toLens()
+    }
+
+}
