fc35b939f00ddb180dafb7461bcf51b83f5c3c37
Diego Gliosca <diego.gliosca@gmail.com>
2024-11-14 20:21:08 +0000

    moved Buyer into its own file

--- auction-service/src/test/kotlin/com/merchantsolutions/AuctionServerTest.kt
+++ auction-service/src/test/kotlin/com/merchantsolutions/AuctionServerTest.kt
@@ -2,22 +2,14 @@ package com.merchantsolutions
 
 import org.http4k.core.*
 import org.junit.jupiter.api.Test
-import com.merchantsolutions.AuctionJson.auto
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import org.http4k.core.Method.GET
 
 class AuctionServerTest {
     private val auctionServer: HttpHandler = auctionApp()
 
     private val seller = Seller(auctionServer)
     private val buyer = Buyer(auctionServer)
-
-    class Buyer(val client: HttpHandler) {
-        fun listAuctions() = activeAuctions(client(Request(GET, "/active-auctions")))
-
-        private val activeAuctions = Body.auto<List<Auction>>().toLens()
-    }
 
     @Test
     fun `seller can register a new product`() {
---
+++ auction-service/src/test/kotlin/com/merchantsolutions/Buyer.kt
@@ -0,0 +1,13 @@
+package com.merchantsolutions
+
+import org.http4k.core.Body
+import org.http4k.core.HttpHandler
+import org.http4k.core.Method
+import org.http4k.core.Request
+import com.merchantsolutions.AuctionJson.auto
+
+class Buyer(val client: HttpHandler) {
+    fun listAuctions() = activeAuctions(client(Request(Method.GET, "/active-auctions")))
+
+    private val activeAuctions = Body.auto<List<Auction>>().toLens()
+}
