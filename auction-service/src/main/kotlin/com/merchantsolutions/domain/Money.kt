1b6650e991ba667178091307ed773de63ebceeaa
Diego Gliosca <diego.gliosca@gmail.com>
2024-11-16 15:00:55 +0000

    clean up

--- auction-service/src/main/kotlin/com/merchantsolutions/domain/Money.kt
+++ auction-service/src/main/kotlin/com/merchantsolutions/domain/Money.kt
@@ -1,10 +1,16 @@
 package com.merchantsolutions.domain
 
 import java.math.BigDecimal
+import java.math.BigDecimal.ZERO
 import java.math.BigInteger
 import java.util.Currency
 
 data class Money(val currency: Currency, val amount: BigDecimal) : Comparable<Money> {
+
+    init {
+        require(value = amount >= ZERO) { "Amount must be non-negative" }
+    }
+
     override fun compareTo(other: Money): Int {
         checkCurrencyMatch(other);
         return this.amount.compareTo(other.amount);
@@ -14,10 +20,6 @@ data class Money(val currency: Currency, val amount: BigDecimal) : Comparable<Mo
         if (this.currency != other.currency) {
             throw IllegalArgumentException ("Cannot compare or operate on Money with different currencies")
         }
-    }
-
-    init {
-        require(amount >= BigDecimal.ZERO) { "Amount must be non-negative" }
     }
 
     companion object {
