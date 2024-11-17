8c7bc732f8f7430286a8a55f18f31dc420ed4938
Diego Gliosca <diego.gliosca@gmail.com>
2024-11-17 19:41:25 +0000

    moved function into main

---
+++ user-service/src/main/kotlin/com/merchantsolutions/drivers/http/UserWeb.kt
@@ -0,0 +1,25 @@
+package com.merchantsolutions.drivers.http
+
+import com.merchantsolutions.UserJson.auto
+import com.merchantsolutions.domain.User
+import com.merchantsolutions.domain.UserId
+import org.http4k.core.Body
+import org.http4k.core.Method.GET
+import org.http4k.core.Response
+import org.http4k.core.Response.Companion.invoke
+import org.http4k.core.Status.Companion.OK
+import org.http4k.core.with
+import org.http4k.routing.RoutingHttpHandler
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+
+fun userApp(): RoutingHttpHandler {
+    return routes(
+        "/is-valid" bind GET to { Response(OK).with(Body.auto<Boolean>().toLens() of true) },
+        "/user-by-token" bind GET to {
+            Response(OK).with(
+                Body.auto<User>().toLens() of User(UserId.of("3d02036f-4087-46e4-8a30-39d234d61de3"))
+            )
+        }
+    )
+}
--- user-service/src/test/kotlin/com/merchantsolutions/UserServiceTest.kt
+++ user-service/src/test/kotlin/com/merchantsolutions/UserServiceTest.kt
@@ -4,18 +4,14 @@ import com.merchantsolutions.UserJson.auto
 import com.merchantsolutions.UserJson.json
 import com.merchantsolutions.domain.User
 import com.merchantsolutions.domain.UserId
+import com.merchantsolutions.drivers.http.userApp
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.http4k.core.Body
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Request.Companion.invoke
-import org.http4k.core.Response
-import org.http4k.core.Status.Companion.OK
 import org.http4k.core.with
-import org.http4k.routing.RoutingHttpHandler
-import org.http4k.routing.bind
-import org.http4k.routing.routes
 import org.junit.jupiter.api.Test
 
 class UserServiceTest {
@@ -34,13 +30,3 @@ class UserServiceTest {
     }
 }
 
-fun userApp(): RoutingHttpHandler {
-    return routes(
-        "/is-valid" bind GET to { Response(OK).with(Body.auto<Boolean>().toLens() of true) },
-        "/user-by-token" bind GET to {
-            Response(OK).with(
-                Body.auto<User>().toLens() of User(UserId.of("3d02036f-4087-46e4-8a30-39d234d61de3"))
-            )
-        }
-    )
-}
